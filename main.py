12d0a02f135bf9f348c2711709b7187ce91d5eb9
qmet1324 <mikhail.melnik.gamedev@gmail.com>
2025-03-30 19:36:33 -0400

    feat: add sorted character list func by counter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import get_num_words
 from stats import get_num_chars
+from stats import get_char_list
+
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -10,10 +12,11 @@ def get_book_text(file_path):
 def main():
     file_contents = get_book_text('./books/frankenstein.txt')
     words_counter = get_num_words(file_contents)
-    # print(file_contents)
-    print('75767 words found in the document')
-    char_counter = get_num_chars(file_contents)
-    print(char_counter)
+    # print(f'{words_counter} words found in the document')
+    char_dict_counter = get_num_chars(file_contents)
+    # print(char_dict_counter)
+    char_list = get_char_list(char_dict_counter)
+    print(char_list)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 def get_num_words(file_str):
     return len(file_str.split())
 
+
 def get_num_chars(file_str):
     char_dict = {}
     char_list = file_str.lower()
@@ -10,3 +11,8 @@ def get_num_chars(file_str):
         else:
             char_dict[char] = 1
     return char_dict
+
+
+def get_char_list(char_dict):
+    char_list = char_dict.items()
+    return sorted(char_list, key=lambda tuple: tuple[1], reverse=True)
