690654c5dbed753875bb262de6a92b6c0cbb6ee0
qmet1324 <mikhail.melnik.gamedev@gmail.com>
2025-03-30 20:23:33 -0400

    feat: add print report func

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_words
 from stats import get_num_chars
 from stats import get_char_list
+from stats import print_report
 
 
 def get_book_text(file_path):
@@ -10,13 +11,12 @@ def get_book_text(file_path):
 
 
 def main():
+    file_path = './books/frankenstein.txt'
     file_contents = get_book_text('./books/frankenstein.txt')
     words_counter = get_num_words(file_contents)
-    # print(f'{words_counter} words found in the document')
     char_dict_counter = get_num_chars(file_contents)
-    # print(char_dict_counter)
     char_list = get_char_list(char_dict_counter)
-    print(char_list)
+    print_report(file_path, words_counter, char_list)
 
 
 main()
--- stats.py
+++ stats.py
@@ -16,3 +16,15 @@ def get_num_chars(file_str):
 def get_char_list(char_dict):
     char_list = char_dict.items()
     return sorted(char_list, key=lambda tuple: tuple[1], reverse=True)
+
+
+def print_report(file_path, words_counter, char_list):
+    print("============ BOOKBOT ============")
+    print(f'Analyzing book found at {file_path}...')
+    print("----------- Word Count ----------")
+    print(f'Found {words_counter} total words')
+    print("--------- Character Count -------")
+    for item in char_list:
+        if item[0].isalpha():
+            print(f'{item[0]}: {item[1]}')
+    print("============= END ===============")
